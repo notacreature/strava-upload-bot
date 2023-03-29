053202ecf315076e9fe94d95aea5ce4d032dbad9
Ivan Ananyev <i.ananyev.formal@gmail.com>
2023-03-29 18:19:44 +0300

    Закончил MVP

--- bot.py
+++ bot.py
@@ -1,17 +1,13 @@
-import logging, requests, time
+import requests, time, asyncio, os
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+from tinydb import TinyDB, Query
 
 BOT_TOKEN = '5976143199:AAHUSdb47E-MCiPwTs1WSGym-5fs2BsmfAo'
 STRAVA_CLIENT_ID = '104425'
 STRAVA_CLIENT_SECRET = '4a4733df7d14ce6b5e6dcc30b0610ad10e555c70'
 REDIRECT_URL = 'http://localhost:8000/'
 
-#Логирование
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
 
 #Обработка /start, /help; Вывод информационного сообщения
 async def info(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -20,29 +16,45 @@ async def info(update: Update, context: ContextTypes.DEFAULT_TYPE):
     inline_keyboard= InlineKeyboardMarkup( [[inline_button]] )
     await update.message.reply_text(f'Для использования бота, разрешите загружать файлы в Strava от вашего имени', reply_markup=inline_keyboard)
 
-#Обмен кода на токен; ПЕРЕДЕЛАТЬ НА РЕГИСТРАЦИЮ ПРИЛОЖЕНИЯ
-async def get_token(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    code = update.message.text
-    url = f'https://www.strava.com/api/v3/oauth/token?client_id={STRAVA_CLIENT_ID}&client_secret={STRAVA_CLIENT_SECRET}&code={code}&grant_type=authorization_code'
-    response = requests.post(url)
-    await update.message.reply_text(response.json()['refresh_token'])
 
 #Загрузка активности в Strava
 async def upload_activity(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = str(update.message.from_user.id)
     file_id = update.message.document.file_id
     file_name = update.message.document.file_name
     file_data = await context.bot.get_file(file_id)
-
-    #Явно задаём токен обновления; ПЕРЕДЕЛАТЬ НА ПОЛУЧЕНИЕ ТОКЕНА ИЗ ХРАНИЛИЩА
-    refresh_token = update.message.caption
-
+    db = TinyDB('userdata.json')
+    user = Query()
+    
     #Получение файла от API Telegram
-    open(file_name, 'wb').write(requests.get(file_data.file_path).content)
+    open(f'storage/{file_name}', 'wb').write(requests.get(file_data.file_path).content)
     time.sleep(20)#Как еще можно дождаться конца скачивания файла?
     
+    #Попытка получить refresh_token из БД; при неуспехе получаем от API и добавляем текущему пользователю в БД
+    try:
+        refresh_token = db.search(user.user_id == user_id)[0]['refresh_token']
+    except (IndexError, KeyError):
+        code = db.search(user['user_id'] == user_id)[0]['auth_code']
+        url = f'https://www.strava.com/api/v3/oauth/token'
+        params = {
+            'client_id': f'{STRAVA_CLIENT_ID}',
+            'client_secret': f'{STRAVA_CLIENT_SECRET}',
+            'grant_type': 'authorization_code',
+            'code': f'{code}'
+            }
+        response = requests.post(url, params=params)
+        refresh_token = response.json()['refresh_token']
+        db.update({'refresh_token': refresh_token}, user['user_id'] == user_id)
+    
     #Обновление токена
-    url = f'https://www.strava.com/api/v3/oauth/token?client_id={STRAVA_CLIENT_ID}&client_secret={STRAVA_CLIENT_SECRET}&grant_type=refresh_token&refresh_token={refresh_token}'
-    response = requests.post(url)
+    url = f'https://www.strava.com/api/v3/oauth/token'
+    params = {
+        'client_id': f'{STRAVA_CLIENT_ID}',
+        'client_secret': f'{STRAVA_CLIENT_SECRET}',
+        'grant_type': 'refresh_token',
+        'refresh_token': f'{refresh_token}'
+        }
+    response = requests.post(url, params=params)
     bearer = response.json()['access_token']
     
     #Загрузка файла в Strava
@@ -56,7 +68,7 @@ async def upload_activity(update: Update, context: ContextTypes.DEFAULT_TYPE):
         'Authorization': f'Bearer {bearer}'
     }
     files = {
-        'file': open(file_name, 'rb')
+        'file': open(f'storage/{file_name}', 'rb')
         }
     response = requests.post(url, params=params, headers=headers, files=files)
     upload_id = response.json()['id_str']
@@ -70,15 +82,21 @@ async def upload_activity(update: Update, context: ContextTypes.DEFAULT_TYPE):
     response = requests.get(url, headers=headers)
     await update.message.reply_text(response.json()['status'])
 
+    try:
+        os.remove(f'storage/{file_name}')
+    except:
+        pass
+
+
 def main() -> None:   
     application = ApplicationBuilder().token(BOT_TOKEN).build()
     
     application.add_handler(CommandHandler('start', info))
     application.add_handler(CommandHandler('help', info))
-    application.add_handler(MessageHandler(filters.TEXT, get_token))
     application.add_handler(MessageHandler(filters.ATTACHMENT, upload_activity))
 
     application.run_polling()
+    
     
 if __name__ == "__main__":
     main()
