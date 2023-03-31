60ab5dd82ba3f616afeec6dad8b15ad30d1c78b0
Ivan Ananyev <redhairawesome@gmail.com>
2023-03-31 04:17:54 +0300

    Пересобрал virtualenv

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Kirov\OneDrive\Рабочий стол\strava\.venv
+set VIRTUAL_ENV=C:\Users\Kirov\OneDrive\Рабочий стол\strava_upload_bot\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
