d860a78afabccd113d9f0d8e79a141a3f5da3e89
Ivan Ananyev <redhairawesome@gmail.com>
2023-04-02 01:37:14 +0300

    Squashed commit of the following:
    commit cfe22490ea61fb08eaf247c9a589e203552f5911
    Author: Ivan Ananyev <redhairawesome@gmail.com>
    Date:   Sun Apr 2 01:34:42 2023 +0300
    
        Исправил ошибку с {} в bot.py
    
    commit 3616428883cf8566074eb6a6537b7d9e38f23318
    Author: Ivan Ananyev <redhairawesome@gmail.com>
    Date:   Sun Apr 2 01:25:00 2023 +0300
    
        Исправил параметры в os.path.join
    
    commit 86418a5c11413b495b97be231f48eeeab5d15a71
    Author: Ivan Ananyev <redhairawesome@gmail.com>
    Date:   Sun Apr 2 00:39:51 2023 +0300
    
        Добавил join в path в файле server
    
    commit 28df77b1fffda953cb8312713cae68fe7477b791
    Author: Ivan Ananyev <redhairawesome@gmail.com>
    Date:   Sun Apr 2 00:34:02 2023 +0300
    
        ПОправил пути в коде server
    
    commit 17d1836f65628b80ad80035a1605fe6f9cd14407
    Author: Ivan Ananyev <redhairawesome@gmail.com>
    Date:   Sun Apr 2 00:28:06 2023 +0300
    
        Поправил пути в коде
    
    commit 0c7036f1c433170d069f50fc1886dc303498e8c6
    Author: Ivan Ananyev <redhairawesome@gmail.com>
    Date:   Sun Apr 2 00:08:21 2023 +0300
    
        Заменил все пути на зависимые от os
    
    commit f8705ede95309416ee92788314bd7c86698f259f
    Author: Ivan Ananyev <redhairawesome@gmail.com>
    Date:   Sat Apr 1 23:56:25 2023 +0300
    
        Добавил импорт os в server
    
    commit 241028dbffeba274f97535fb22821efb29d01b3f
    Author: Ivan Ananyev <redhairawesome@gmail.com>
    Date:   Sat Apr 1 23:51:45 2023 +0300
    
        Заменил путь к конфигу на относительный для os
    
    commit 4597325198d10b2b447c8e17a912f776f3e97797
    Author: Ivan Ananyev <redhairawesome@gmail.com>
    Date:   Sat Apr 1 20:15:19 2023 +0300
    
        Изменил пути файлов в скриптах

--- src/server.py
+++ src/server.py
@@ -1,10 +1,11 @@
-import configparser
+import os, configparser
 from http import server
 from socketserver import BaseServer, TCPServer
 from tinydb import TinyDB, Query
 
 config = configparser.ConfigParser()
-config.read('../settings.ini')
+config.read(os.path.join(os.path.dirname(__file__), '..', 'settings.ini'))
+
 
 #Создаем свой класс обработчика запросов, наследуя от SimpleHTTPRequestHandler
 class MyHTTPRequestHandler(server.SimpleHTTPRequestHandler):
@@ -26,7 +27,7 @@ class MyHTTPRequestHandler(server.SimpleHTTPRequestHandler):
         
         #TODO Баг: если база пуста, вызов if возвращает ошибку, потому что в ней нет ключа 'user_id'
         #Сохраняем параметры в хранилище
-        db = TinyDB('../storage/userdata.json')
+        db = TinyDB(os.path.join(os.path.dirname(__file__), '..', 'storage', 'userdata.json'))
         user = Query()
         if db.contains(user['user_id'] == params['user_id']):
             db.update({'auth_code': params['code']}, user['user_id'] == params['user_id'])
