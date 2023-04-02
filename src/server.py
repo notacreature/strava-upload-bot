d2ba8d2e9690a70521dd331585f6ad481d098662
Ivan Ananyev <redhairawesome@gmail.com>
2023-04-02 05:04:58 +0300

    Стабильная версия 1.1

--- src/server.py
+++ src/server.py
@@ -1,4 +1,7 @@
-import os, configparser
+#TODO Предусмотреть отмену галочки при выдаче доступа приложению
+#TODO Баг: если база пуста, вызов if возвращает ошибку, потому что в ней нет ключа 'user_id'
+
+import os, configparser, requests
 from http import server
 from socketserver import BaseServer, TCPServer
 from tinydb import TinyDB, Query
@@ -7,11 +10,10 @@ config = configparser.ConfigParser()
 config.read(os.path.join(os.path.dirname(__file__), '..', 'settings.ini'))
 
 
-#Создаем свой класс обработчика запросов, наследуя от SimpleHTTPRequestHandler
+#Создаем класс обработчика запросов, наследуя от SimpleHTTPRequestHandler
 class ParamsHTTPRequestHandler(server.SimpleHTTPRequestHandler):
     #Переопределяем метод do_GET()
     def do_GET(self):
-        #Получаем путь и параметры запроса
         path = self.path
         params = {}
         url = config['Telegram']['BOT_URL']
@@ -25,7 +27,15 @@ class ParamsHTTPRequestHandler(server.SimpleHTTPRequestHandler):
         self.send_header('Location', url)
         self.end_headers()
         
-        #TODO Баг: если база пуста, вызов if возвращает ошибку, потому что в ней нет ключа 'user_id'
+        #Отвечаем в чат
+        url = f'https://api.telegram.org/bot{config["Telegram"]["BOT_TOKEN"]}/sendMessage'
+        params = {
+            'chat_id': params['user_id'],
+            'text': 'Отлично! Теперь я могу загружать активности в Strava.\nПрисылайте мне файлы `.fit`, `.tcx` или `.gpx` и я буду их публиковать 🚴‍♂️🏃‍♀️',
+            'parse_mode': 'Markdown'
+            }
+        requests.post(url, params=params)
+
         #Сохраняем параметры в хранилище
         db = TinyDB(os.path.join(os.path.dirname(__file__), '..', 'storage', 'userdata.json'))
         user = Query()
@@ -34,12 +44,8 @@ class ParamsHTTPRequestHandler(server.SimpleHTTPRequestHandler):
         else:
             db.insert({'user_id': params['user_id'], 'auth_code': params['code']})
 
-#Создаем объект сервера, используя класс TCPServer из модуля socketserver
+
+#Создаем и запускаем TCPServer
 port = int(config['Server']['PORT'])
-my_server = TCPServer(("", port), ParamsHTTPRequestHandler)
-
-#Выводим информацию о запуске сервера
-print(f"HTTP server running on port {port}")
-
-#Запускаем бесконечный цикл обработки запросов
-BaseServer.serve_forever(my_server)
+tcp_server = TCPServer(('', port), ParamsHTTPRequestHandler)
+BaseServer.serve_forever(tcp_server)
