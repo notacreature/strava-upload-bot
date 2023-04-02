4a4b90bd609940eceb9dc941bf3a50a080a47c3a
Ivan Ananyev <redhairawesome@gmail.com>
2023-04-02 21:54:46 +0300

    Добавил обработку всех состояний загрузки; отформатировал код

--- src/server.py
+++ src/server.py
@@ -1,5 +1,5 @@
-#TODO Предусмотреть отмену галочки при выдаче доступа приложению
-#TODO Баг: если база пуста, вызов if возвращает ошибку, потому что в ней нет ключа 'user_id'
+# TODO Предусмотреть отмену галочки при выдаче доступа приложению
+# TODO Баг: если база пуста, вызов if возвращает ошибку, потому что в ней нет ключа 'user_id'
 
 import os, configparser, requests
 from http import server
@@ -7,45 +7,45 @@ from socketserver import BaseServer, TCPServer
 from tinydb import TinyDB, Query
 
 config = configparser.ConfigParser()
-config.read(os.path.join(os.path.dirname(__file__), '..', 'settings.ini'))
+config.read(os.path.join(os.path.dirname(__file__), "..", "settings.ini"))
 
 
-#Создаем класс обработчика запросов, наследуя от SimpleHTTPRequestHandler
+# Создаем класс обработчика запросов, наследуя от SimpleHTTPRequestHandler
 class ParamsHTTPRequestHandler(server.SimpleHTTPRequestHandler):
-    #Переопределяем метод do_GET()
+    # Переопределяем метод do_GET()
     def do_GET(self):
         path = self.path
         params = {}
-        url = config['Telegram']['BOT_URL']
+        url = config["Telegram"]["BOT_URL"]
         if "?" in path:
             path, query = path.split("?", 1)
             for pair in query.split("&"):
                 key, value = pair.split("=", 1)
                 params[key] = value
-        
+
         self.send_response(301)
-        self.send_header('Location', url)
+        self.send_header("Location", url)
         self.end_headers()
-        
-        #Отвечаем в чат
-        url = f'https://api.telegram.org/bot{config["Telegram"]["BOT_TOKEN"]}/sendMessage'
+
+        # Отвечаем в чат
+        url = (f'https://api.telegram.org/bot{config["Telegram"]["BOT_TOKEN"]}/sendMessage')
         params = {
-            'chat_id': params['user_id'],
-            'text': 'Отлично! Теперь я могу загружать активности в Strava.\nПрисылайте мне файлы `.fit`, `.tcx` или `.gpx` и я буду их публиковать 🚴‍♂️🏃‍♀️',
-            'parse_mode': 'Markdown'
-            }
+            "chat_id": params["user_id"],
+            "text": "Отлично! Теперь я могу загружать активности в Strava.\nПрисылайте мне файлы `.fit`, `.tcx` или `.gpx` и я буду их публиковать 🚴‍♂️🏃‍♀️",
+            "parse_mode": "Markdown",
+        }
         requests.post(url, params=params)
 
-        #Сохраняем параметры в хранилище
-        db = TinyDB(os.path.join(os.path.dirname(__file__), '..', 'storage', 'userdata.json'))
+        # Сохраняем параметры в хранилище
+        db = TinyDB(os.path.join(os.path.dirname(__file__), "..", "storage", "userdata.json"))
         user = Query()
-        if db.contains(user['user_id'] == params['user_id']):
-            db.update({'auth_code': params['code']}, user['user_id'] == params['user_id'])
+        if db.contains(user["user_id"] == params["user_id"]):
+            db.update({"auth_code": params["code"]}, user["user_id"] == params["user_id"])
         else:
-            db.insert({'user_id': params['user_id'], 'auth_code': params['code']})
+            db.insert({"user_id": params["user_id"], "auth_code": params["code"]})
 
 
-#Создаем и запускаем TCPServer
-port = int(config['Server']['PORT'])
-tcp_server = TCPServer(('', port), ParamsHTTPRequestHandler)
+# Создаем и запускаем TCPServer
+port = int(config["Server"]["PORT"])
+tcp_server = TCPServer(("", port), ParamsHTTPRequestHandler)
 BaseServer.serve_forever(tcp_server)
